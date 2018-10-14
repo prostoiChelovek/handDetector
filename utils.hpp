2e9377333042fdaade522d50c2c403cba66d5c64
prostoiChelovek <mrBlaze@yandex.ru>
2018-10-14 20:57:09 +0500

    move 'innerAngle' to 'utils.hpp'

---
+++ utils.hpp
@@ -0,0 +1,44 @@
+#ifndef HD_UTILS_HPP
+#define HD_UTILS_HPP
+
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace cv;
+
+// https://picoledelimao.github.io/blog/2015/11/15/fingertip-detection-on-opencv/
+float innerAngle(float px1, float py1, float px2, float py2, float cx1, float cy1)
+{
+	float dist1 = sqrt((px1 - cx1) * (px1 - cx1) + (py1 - cy1) * (py1 - cy1));
+	float dist2 = sqrt((px2 - cx1) * (px2 - cx1) + (py2 - cy1) * (py2 - cy1));
+	float Ax, Ay;
+	float Bx, By;
+	float Cx, Cy;
+	//find closest point to C
+	//printf("dist = %lf %lf\n", dist1, dist2);
+	Cx = cx1;
+	Cy = cy1;
+	if (dist1 < dist2)
+	{
+		Bx = px1;
+		By = py1;
+		Ax = px2;
+		Ay = py2;
+	}
+	else
+	{
+		Bx = px2;
+		By = py2;
+		Ax = px1;
+		Ay = py1;
+	}
+	float Q1 = Cx - Ax;
+	float Q2 = Cy - Ay;
+	float ptStart = Bx - Ax;
+	float ptEnd = By - Ay;
+	float A = acos((ptStart * Q1 + ptEnd * Q2) / (sqrt(ptStart * ptStart + ptEnd * ptEnd) * sqrt(Q1 * Q1 + Q2 * Q2)));
+	A = A * 180 / CV_PI;
+	return A;
+}
+
+#endif // !HD_UTILS_HPP
